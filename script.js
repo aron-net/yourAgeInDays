32f1c4075c500ff5ba568274838a3de400b2e818
aron-helu <101916235+aron-helu@users.noreply.github.com>
2022-04-22 00:40:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function yourAgeInDays() {
     let birthYear = prompt("Which year were you born... Good Friend? ")
     let ageYear = (new Date().getFullYear() - birthYear) * 365;
-    let textCreate = document.createElement('h1');
+    let textCreate = document.createElement('h2');
     let textAnswer = document.createTextNode('You are ' + ageYear + ' days old.');
     textCreate.setAttribute('id', 'textCreated');
     textCreate.append(textAnswer);
