f49fcd402461b75f4a06757f2a8e34a5ebb51667
aron-helu <aronmilahela@gmail.com>
2022-04-22 00:31:05 +0300

    feat: The new feature being added #1

---
+++ script.js
@@ -0,0 +1,14 @@
+function yourAgeInDays() {
+    let birthYear = prompt("Which year were you born... Good Friend? ")
+    let ageYear = (new Date().getFullYear() - birthYear) * 365;
+    let textCreate = document.createElement('h1');
+    let textAnswer = document.createTextNode('You are ' + ageYear + ' days old.');
+    textCreate.setAttribute('id', 'textCreated');
+    textCreate.append(textAnswer);
+    document.getElementById('textResult').appendChild(textCreate);
+    
+}
+
+function resetSite() {
+    document.getElementById('textCreated').remove();
+}
